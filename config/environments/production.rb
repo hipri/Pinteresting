a2605b60014933e2b296b3eb310376cf8b9897c7
Hipri Joven <fatima.hiprenoridel@gmail.com>
2021-02-08 10:23:48 +0800

    Changed app name to hip-pinteresting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'pinterestingph.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'hip-pinteresting.herokuapp.com'}
 
   config.paperclip_defaults = {
     storage: :s3,
